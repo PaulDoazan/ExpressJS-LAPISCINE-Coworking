58d1a4c38befc378df8e052f757c2ea926a6c41e
pdoazan <paul.doazan@gmail.com>
2023-04-03 17:33:22 +0200

    Add optional parameters in query

--- app.js
+++ app.js
@@ -5,23 +5,18 @@ const port = 3000
 const coworkings = require('./mock-coworkings');
 
 app.get('/api/coworkings', (req, res) => {
-    let sentence = ''
-    // coworkings.forEach((coworking) => {
-    //     sentence += coworking.name + ' '
-    // })
+    // Renvoyer tous les coworkings au format json, uniquement ceux dont la surface est supérieure à 500
+    console.log(req.query)
+    const limit = req.query.limit || 200
+    const result = coworkings.filter(element => element.superficy > limit);
 
-    coworkings.map((coworking) => {
-        sentence += coworking.name + ' '
-    })
-    console.log(`Je viens de faire une requête get sur mon app`)
-    res.send(sentence)
+    res.json(result)
 })
 
 app.get('/api/coworkings/:id', (req, res) => {
     // Afficher le nom du coworking qui correspond à l'id en paramètre
-    console.log(coworkings[0].id, req.params.id)
     let myCoworking = coworkings.find((coworking) => { return coworking.id === Number(req.params.id) })
-    res.send(`Nom du coworking : ${myCoworking.name}`)
+    res.json(myCoworking)
 })
 
 app.listen(port, () => {
