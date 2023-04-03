c11ec9387b71aca9646b819876dff0c235155686
pdoazan <paul.doazan@gmail.com>
2023-04-03 16:16:19 +0200

    Get specific coworking on id parameter

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express()
-const port = 3005
+const port = 3000
 
-app.get('/monChemin', (req, res) => {
+const coworkings = require('./mock-coworkings');
+
+app.get('/api/coworkings', (req, res) => {
+    let sentence = ''
+    // coworkings.forEach((coworking) => {
+    //     sentence += coworking.name + ' '
+    // })
+
+    coworkings.map((coworking) => {
+        sentence += coworking.name + ' '
+    })
     console.log(`Je viens de faire une requête get sur mon app`)
-    res.send('Hello World !')
+    res.send(sentence)
+})
+
+app.get('/api/coworkings/:id', (req, res) => {
+    // Afficher le nom du coworking qui correspond à l'id en paramètre
+    console.log(coworkings[0].id, req.params.id)
+    let myCoworking = coworkings.find((coworking) => { return coworking.id === Number(req.params.id) })
+    res.send(`Nom du coworking : ${myCoworking.name}`)
 })
 
 app.listen(port, () => {
