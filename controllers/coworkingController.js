c0dc1ebb6cd6112624d35cbc8dc9268089709209
Paul Doazan <paul.doazan@edumedia-sciences.com>
2023-04-06 17:40:42 +0200

    Replace CRUD controllers with database connections

--- controllers/coworkingController.js
+++ controllers/coworkingController.js
@@ -5,54 +5,51 @@ const { Coworking } = require('../db/sequelize')
 exports.findAllCoworkings = (req, res) => {
     // Renvoyer tous les coworkings au format json, uniquement ceux dont la surface est supérieure à 500
     const limit = req.query.limit || 200
-    const result = coworkings.filter(element => element.superficy > limit);
+    // const result = coworkings.filter(element => element.superficy > limit);
 
-    const msg = `La liste des coworkings a bien été retournée.`
-    res.json({ message: msg, data: coworkings })
+    // utiliser le model Coworking pour lister tous les coworkings existants dans la bdd... findAll()
+    Coworking.findAll()
+        .then((elements)=>{
+            const msg = 'La liste des coworkings a bien été récupérée en base de données.'
+            res.json({message: msg, data: elements})
+        })
+        .catch((error) => {
+            const msg = 'Une erreur est survenue.'
+            res.json({message: msg})
+        })
 }
 
 exports.findCoworkingByPk = (req, res) => {
-    // Afficher le nom du coworking qui correspond à l'id en paramètre
-    let myCoworking = coworkings.find((coworking) => { return coworking.id === Number(req.params.id) })
-
-    let result;
-    if (myCoworking) {
-        const msg = `Le coworking n°${myCoworking.id} a bien été trouvé.`
-        result = { message: msg, data: myCoworking }
-    } else {
-        const msg = `Aucun coworking ne correspond à l'identifiant ${req.params.id}.`
-        result = { message: msg, data: {} }
-    }
-
-    res.json(result)
+    // Afficher le coworking correspondant à l'id en params, en le récupérant dans la bdd     findByPk()
+    Coworking.findByPk(req.params.id)
+        .then(() => {
+            const msg = `Le coworking n° a bien été récupéré.`
+            res.json({message: msg})
+        })
+        .catch(() => {
+            const msg = `Aucun coworking ne correspond à l'id n°${req.params.id}.`
+            res.json({message: msg})
+        })
 }
 
 exports.updateCoworking = (req, res) => {
-    // récupérer dans le tableau coworkings le coworking qui correspond à l'id dans les params de la requête
-    let coworkingToModify = coworkings.find(coworking => coworking.id == req.params.id)
-
-    // Si le coworking n'existe pas, on renvoie une erreur 404 avec un message
-    if (!coworkingToModify) {
-        const msg = `Le coworking n°${req.params.id} n'existe pas.`
-        return res.status(404).json({ message: msg })
-    }
-
-    coworkingToModify = { ...coworkingToModify, ...req.body }
-
-    // remplacer dans le tableau coworkings l'ancien coworking 16 avec coworkingToModify
-
-    // for (let i = 0; i < coworkings.length; i++) {
-    //     if (coworkings[i].id == coworkingToModify.id) {
-    //         coworkings[i] = coworkingToModify
-    //         break;
-    //     }
-    // }
-
-    let index = coworkings.findIndex((el) => el.id == coworkingToModify.id);
-    coworkings[index] = coworkingToModify;
-
-    console.log(req.body)
-    res.json(coworkings)
+    // Modifier le coworking en base de données qui correspond à l'id spécifé dans les params
+    Coworking.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    }).then((coworking) => {
+        if(coworking === null){
+            const msg = "Le coworking demandé n'existe pas."
+            res.json({message: msg})
+        } else {
+            const msg = "Le coworking a bien été modifié."
+            res.json({message: msg, data: coworking})
+        }
+    }).catch(() => {
+        const msg = "Impossible de mettre à jour le coworking."
+        res.json({message: msg})
+    })
 }
 
 exports.deleteCoworking = (req, res) => {
@@ -95,5 +92,4 @@ exports.createCoworking = (req, res) => {
         const msg = 'Un coworking a bien été ajouté.'
         res.json({ message: msg, data: newCoworking })
     }).catch(error => res.json(error))
-    res.json(req.body)
 }
