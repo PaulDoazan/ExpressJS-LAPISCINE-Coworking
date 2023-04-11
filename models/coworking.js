dda5098383b89e809f6e1c1119a6bf0f38f529f4
Paul Doazan <paul.doazan@edumedia-sciences.com>
2023-04-11 14:07:04 +0200

    Add validations on Coworking model

--- models/coworking.js
+++ models/coworking.js
@@ -8,21 +8,54 @@ module.exports = (sequelize, DataTypes) => {
         name: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: {
+                msg: 'Le nom est déjà pris'
+            },
+            validate: {
+                notEmpty: {
+                  msg: 'Ce champ ne peut pas être vide.'
+                }
+              }
         },
         picture: {
             type: DataTypes.STRING,
         },
+
+        // Exo, ajouter la validation de nombres entiers
         superficy: {
             type: DataTypes.INTEGER,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isInt: {
+                  msg: 'La superficie doit être un entier.'
+                }
+              }
         },
+        // idem
         capacity: {
             type: DataTypes.INTEGER,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isInt: {
+                  msg: 'Le nombre de postes doit être un entier.'
+                }
+              }
         },
+        // Exo, ajouter la validation custom du prix, au moins un des 3 prix doit être renseigné
         price: {
             type: DataTypes.JSON,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isPriceValid(value) {
+                    if(value.hasOwnProperty('hour') && value.hasOwnProperty('day') && value.hasOwnProperty('month')){
+                        if ( value.hour === null && value.day === null && value.month === null  ) {
+                            throw new Error("Au moins un des 3 tarifs doit être spécifié"); 
+                          }
+                    } else {
+                        throw new Error("La syntaxe des données est incorrecte.");
+                    }
+                }
+              }
         },
         address: {
             type: DataTypes.JSON,
